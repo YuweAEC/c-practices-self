ebc4108c1dd0620ac72c7d2c9527017a0dd8724d
Yuvraj Singh <yuwe18b@gmail.com>
2023-05-19 22:42:13 +0530

    add new codes and file

---
+++ prac2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void func1()
+{
+    printf("\n lets talk about functions..");
+}
+
+void main()
+{
+    printf("\n welcome to functions..");
+    func1();
+}
+
+//void func1()
+
