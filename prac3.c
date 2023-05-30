bc48d481c41a1030b312d9a301ef5bac29fefe55
Yuvraj Singh <yuwe18b@gmail.com>
2023-05-30 23:22:33 +0530

    add nw codes

--- prac3.c
+++ prac3.c
@@ -1,3 +1,4 @@
+/*
 #include<stdio.h>
 
 void formain();
@@ -12,4 +13,27 @@ void formain()
     printf("Learning C programing once again");
     main();
 }
+*/
 
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int avg, sum = 0;
+    int i;
+    int marks[30]; /* array declaration */
+    for (i = 0; i <= 29; i++)
+    {
+        printf("\nEnter marks ");
+        scanf("%d", &marks[i]); /* store data in array */
+    }
+    for (i = 0; i <= 29; i++)
+        sum = sum + marks[i]; /* read data from an array*/
+    avg = sum / 30;
+    printf("\nAverage marks = %d", avg);
+}
+
+/**
+ * 28 -> 
+*/
