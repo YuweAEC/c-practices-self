9923a031f70f410293bec06d67aa3dd9204d2db5
Yuvraj Singh <yuwe18b@gmail.com>
2023-05-30 23:26:50 +0530

    update new codes

--- prac3.c
+++ prac3.c
@@ -23,17 +23,19 @@ int main()
     int avg, sum = 0;
     int i;
     int marks[30]; /* array declaration */
+    
     for (i = 0; i <= 29; i++)
     {
         printf("\nEnter marks ");
         scanf("%d", &marks[i]); /* store data in array */
     }
+    
     for (i = 0; i <= 29; i++)
         sum = sum + marks[i]; /* read data from an array*/
-    avg = sum / 30;
+        avg = sum / 30;
     printf("\nAverage marks = %d", avg);
 }
 
 /**
- * 28 -> 
+ * 28 ->
 */
