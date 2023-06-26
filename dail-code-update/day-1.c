d884b85fccb9a725d1fad6a2853e078d1ec164ab
Yuvraj Singh <yuwe18b@gmail.com>
2023-06-26 20:59:31 +0530

    add new codes in new daily branch

---
+++ dail-code-update/day-1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+    int x=4, y=0, z;
+    while(x>=0)
+    {
+        if(x==)
+            break;
+        else
+            printf("\n %d %d", x, y);
+        x--;
+        y++;
+    }
+}
