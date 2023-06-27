9541fc74a30104a43ffcf25fb76df04719bf780f
Yuvraj Singh <yuwe18b@gmail.com>
2023-06-27 16:22:45 +0530

    add new codes

--- dail-code-update/day-2.c
+++ dail-code-update/day-2.c
@@ -32,3 +32,39 @@ int main()
 #include <stdio.h>
 #include <conio.h>
 
+int main()
+{
+    int m1, m2, m3, m4, m5, per;
+    
+    printf("enter marks in five subjects");
+    scanf("%d %d %d %d %d", &m1, &m2, &m3, &m4, &m5);
+    per=(m1+m2+m3+m4+m5)/500*100;
+
+    if(per>=60)
+    {
+        printf("first division");
+    }
+
+    else
+    {
+        if (per>=50)
+        {
+            printf("second division");
+        }
+
+        else
+        {
+            if (per>=40)
+            {
+                printf("third division");
+            }
+            
+            else
+            {
+                printf("fail");
+            }
+            
+        }
+        
+    }
+}
