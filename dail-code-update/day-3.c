6911eb31f5f5b52ae0773625ef421b8f30dfc5e3
Yuvraj Singh <yuwe18b@gmail.com>
2023-06-29 16:23:38 +0530

    merge econflict changes

--- dail-code-update/day-3.c
+++ dail-code-update/day-3.c
@@ -1,36 +1,101 @@
 #include <stdio.h>
 #include<conio.h>
 
+int main()
+{
+    int i, space, rows, k = 0, count = 0, count1 = 0;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; ++i)
+    {
+        for (space = 1; space <= rows - i; ++space)
+        {
+            printf("  ");
+            ++count;
+        }
+        while (k != 2 * i - 1)
+        {
+            if (count <= rows - 1)
+            {
+                printf("%d ", i + k);
+                ++count;
+            }
+            else
+            {
+                ++count1;
+                printf("%d ", (i + k - 2 * count1));
+            }
+            ++k;
+        }
+        count1 = count = k = 0;
+        printf("\n");
+    }
+    return 0;
+}
+
+
+//pascals triangle
+
+
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i, space, rows, k = 0, count = 0, count1 = 0;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; ++i)
+    {
+        for (space = 1; space <= rows - i; ++space)
+        {
+            printf("  ");
+            ++count;
+        }
+        while (k != 2 * i - 1)
+        {
+            if (count <= rows - 1)
+            {
+                printf("%d ", i + k);
+                ++count;
+            }
+            else
+            {
+                ++count1;
+                printf("%d ", (i + k - 2 * count1));
+            }
+            ++k;
+        }
+        count1 = count = k = 0;
+        printf("\n");
+    }
+    return 0;
+}
+
+
+//pascals triangle
+
+
 // #include <stdio.h>
 // #include<conio.h>
 
 // int main()
 // {
-//     int i, space, rows, k = 0, count = 0, count1 = 0;
+//     int rows, coef = 1, space, i, j;
 //     printf("Enter the number of rows: ");
 //     scanf("%d", &rows);
-//     for (i = 1; i <= rows; ++i)
+//     for (i = 0; i < rows; i++)
 //     {
-//         for (space = 1; space <= rows - i; ++space)
-//         {
+//         for (space = 1; space <= rows - i; space++)
 //             printf("  ");
-//             ++count;
-//         }
-//         while (k != 2 * i - 1)
+//         for (j = 0; j <= i; j++)
 //         {
-//             if (count <= rows - 1)
-//             {
-//                 printf("%d ", i + k);
-//                 ++count;
-//             }
+//             if (j == 0 || i == 0)
+//                 coef = 1;
 //             else
-//             {
-//                 ++count1;
-//                 printf("%d ", (i + k - 2 * count1));
-//             }
-//             ++k;
+//                 coef = coef * (i - j + 1) / j;
+//             printf("%4d", coef);
 //         }
-//         count1 = count = k = 0;
 //         printf("\n");
 //     }
 //     return 0;
