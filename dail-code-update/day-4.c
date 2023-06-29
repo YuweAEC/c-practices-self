eecaddd52e780462ddfce573f84aa3820cb1e24a
Yuvraj Singh <yuwe18b@gmail.com>
2023-06-29 16:42:19 +0530

    comment uperlower codes and open new prime codes

--- dail-code-update/day-4.c
+++ dail-code-update/day-4.c
@@ -1,28 +1,29 @@
-// ocunt upc and lwc and other characters
+// // ocunt upc and lwc and other characters
 
-#include <stdio.h>
-#include <ctype.h>
+// #include <stdio.h>
+// #include <ctype.h>
 
-int main(void)
-{
-    int chr;
-    int uppercase_ctr = 0, lowercase_ctr = 0, other_ctr = 0;
-    printf("\nInput characters: On Linux systems and OS X EOF is CTRL+D. For Windows EOF is CTRL+Z.\n");
-    while ((chr = getchar()) != EOF)
-    {
-        if (isupper(chr))
-            uppercase_ctr++;
-        else if (islower(chr))
-            lowercase_ctr++;
-        else
-            other_ctr++;
-    }
+// int main(void)
+// {
+//     int chr;
+//     int uppercase_ctr = 0, lowercase_ctr = 0, other_ctr = 0;
+//     printf("\nInput characters: for other interface is CTRL+D. For Windows is CTRL+Z.\n");
+//     while ((chr = getchar()) != EOF)
+//     {
+//         if (isupper(chr))
+//             uppercase_ctr++;
+//         else if (islower(chr))
+//             lowercase_ctr++;
+//         else
+//             other_ctr++;
+//     }
 
-    printf("\nUppercase letters: %d\n", uppercase_ctr);
-    printf("Lowercase letters: %d\n", lowercase_ctr);
-    printf("Other characters: %d\n", other_ctr);
-    return 0;
-}
+//     printf("\nUppercase letters: %d\n", uppercase_ctr);
+//     printf("Lowercase letters: %d\n", lowercase_ctr);
+//     printf("Other characters: %d\n", other_ctr);
+
+//     return 0;
+// }
 
 
 // C Program to print all prime factors
