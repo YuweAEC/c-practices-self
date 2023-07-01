3b2e438f783825404fa85e9576a6c29b1f4aa7de
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-01 17:04:15 +0530

    add array pointer code

---
+++ dail-code-update/day-6.c
@@ -0,0 +1,29 @@
+int main()
+{
+    int array[] = {3, 5, 1, 4, 6, 2};
+    int done = 0;
+    int i;
+
+    while (done == 0)
+    {
+        done = 1;
+        for (i = 0; i <= 4; i++)
+        {
+            if (array[i] < array[i + 1])
+            {
+                swap(&array[i], &array[i + 1]);
+                done = 0;
+            }
+        }
+        for (i = 5; i >= 1; i--)
+        {
+            if (array[i] > array[i - 1])
+            {
+                swap(&array[i], &array[i - 1]);
+                done = 0;
+            }
+        }
+    }
+
+    printf("%d", array[3]);
+}
