f8da214a2e2612d19b831ac72ac61657baf0d589
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-01 19:34:54 +0530

    add revised swap code and commented the privious

--- dail-code-update/day-6.c
+++ dail-code-update/day-6.c
@@ -1,6 +1,42 @@
 #include<stdio.h>
 #include<conio.h>
 
+// int main()
+// {
+//     int array[] = {3, 5, 1, 4, 6, 2};
+//     int done = 0;
+//     int i;
+
+//     while (done == 0)
+//     {
+//         done = 1;
+//         for (i = 0; i <= 4; i++)
+//         {
+//             if (array[i] < array[i + 1])
+//             {
+//                 swap(&array[i], &array[i + 1]);
+//                 done = 0;
+//             }
+//         }
+//         for (i = 5; i >= 1; i--)
+//         {
+//             if (array[i] > array[i - 1])
+//             {
+//                 swap(&array[i], &array[i - 1]);
+//                 done = 0;
+//             }
+//         }
+//     }
+//     printf("%d", array[3]);
+// }
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main()
 {
     int array[] = {3, 5, 1, 4, 6, 2};
@@ -28,4 +64,6 @@ int main()
         }
     }
     printf("%d", array[3]);
+
+    return 0;
 }
