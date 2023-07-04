734d042c6746416771137ce3f38bf33db275a025
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-04 22:22:17 +0530

    modify: code of macro

--- daily-code-update/day-8.c
+++ daily-code-update/day-8.c
@@ -1,15 +1,27 @@
 #include<stdio.h>
 #include<conio.h>
+// #define AND &&
+// #define OR ||
+
+// void main()
+// {
+//     int f = 1, x = 4, y = 90;
+    
+//     if ((f < 5) AND(x <= 20 OR y <= 45))
+//         printf("\nYour PC will always work fine...");
+    
+//     else
+//         printf("\nIn front of the maintenance man");
+// }
+
 #define AND &&
-#define OR ||
+#define ARANGE (a > 25 AND a < 50)
 
 void main()
 {
-    int f = 1, x = 4, y = 90;
-    
-    if ((f < 5) AND(x <= 20 OR y <= 45))
-        printf("\nYour PC will always work fine...");
-    
+    int a = 30;
+    if (ARANGE)
+        printf("within range");
     else
-        printf("\nIn front of the maintenance man");
+        printf("out of range");
 }
