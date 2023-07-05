97116c18b96f9809f124d0221f378396c771893e
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-05 21:16:37 +0530

    refactor: function code to new file

--- daily-code-update/day-8.c
+++ daily-code-update/day-8.c
@@ -1,30 +1,30 @@
 #include<stdio.h>
 #include<conio.h>
-// #define AND &&
-// #define OR ||
+#define AND &&
+#define OR ||
 
-// void main()
-// {
-//     int f = 1, x = 4, y = 90;
+void main()
+{
+    int f = 1, x = 4, y = 90;
     
-//     if ((f < 5) AND(x <= 20 OR y <= 45))
-//         printf("\nYour PC will always work fine...");
+    if ((f < 5) AND(x <= 20 OR y <= 45))
+        printf("\nYour PC will always work fine...");
     
-//     else
-//         printf("\nIn front of the maintenance man");
-// }
+    else
+        printf("\nIn front of the maintenance man");
+}
 
-// #define AND &&
-// #define ARANGE (a > 25 AND a < 50)
+#define AND &&
+#define ARANGE (a > 25 AND a < 50)
 
-// void main()
-// {
-//     int a = 30;
-//     if (ARANGE)
-//         printf("within range");
-//     else
-//         printf("out of range");
-// }
+void main()
+{
+    int a = 30;
+    if (ARANGE)
+        printf("within range");
+    else
+        printf("out of range");
+}
 
 #define FOUND printf("The Yankee Doodle Virus");
 
---
+++ daily-code-update/function.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <conio.h>
+#define UPPER 50
+#define PRINTNAME printf("\nYuvi");
+#define ADD5(int) {return a + 5};
+#define AND &&
+#define OR ||
+#define AND &&
+#define ARANGE (a > 25 AND a < 50)
+#define FOUND printf("The Yankee Doodle Virus");
+
+int add5(int);
+
+void main()
+{
+    int x, y;
+    printf("%d", UPPER);
+
+    PRINTNAME;
+
+    x = add5(5);
+    y = add5(100);
+    printf("\n%d", x);
+    printf("\n%d", y);
+}
+
+int add5(int a) { return a + 5; }
+
+void main()
+{
+    int f = 1, x = 4, y = 90;
+
+    if ((f < 5) AND(x <= 20 OR y <= 45))
+        printf("\nYour PC will always work fine...");
+
+    else
+        printf("\nIn front of the maintenance man");
+}
+
+
+void main()
+{
+    int a = 30;
+    if (ARANGE)
+        printf("within range");
+    else
+        printf("out of range");
+}
+
+void main()
+{
+    char signature;
+    if (signature == 'Y')
+        FOUND
+    else
+        printf("Safe... as yet !");
+}
+
