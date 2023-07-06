f6d4e1f8dfcb7fc638345b2de411d418394b6692
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-06 13:34:41 +0530

    fix: function name in function.c file

--- daily-code-update/function.c
+++ daily-code-update/function.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <conio.h>
 #define UPPER 50
-#define PRINTNAME printf("\nfunc1- Yuvi");
+#define PRINTNAME printf("\nmacro1- Yuvi");
 #define ADD5(int) {return a + 5};
 #define AND &&
 #define OR ||
 #define ARANGE (a > 25 AND a < 50)
 #define FOUND printf("The Yankee Doodle Virus");
 
-void func1();
-void func2();
-void func3();
-void func4();
+void macro1();
+void macro2();
+void macro3();
+void macro4();
 
 void main()
 {
-    func1();
-    func2();
-    func3();
-    func4();
+    macro1();
+    macro2();
+    macro3();
+    macro4();
 }
 
 int add5(int);
 
-void func1()
+void macro1()
 {
     int x, y;
     printf("%d", UPPER);
@@ -32,40 +32,40 @@ void func1()
 
     x = add5(5);
     y = add5(100);
-    printf("\nfunc1- %d\t", x);
-    printf("\nfunc1- %d\t", y);
+    printf("\nmacro1- %d", x);
+    printf("\nmacro1- %d", y);
 }
 
 int add5(int a) { return a + 5; }
 
-void func2()
+void macro2()
 {
     int f = 1, x = 4, y = 90;
 
     if ((f < 5) AND(x <= 20 OR y <= 45))
-        printf("\nfunc2- Your PC will always work fine...\t");
+        printf("\nmacro2- Your PC will always work fine...");
 
     else
         printf("\nIn front of the maintenance man");
 }
 
 
-void func3()
+void macro3()
 {
     int a = 30;
     if (ARANGE)
-        printf("\nfunc3- within range\t");
+        printf("\nmacro3- within range");
     else
         printf("out of range");
 }
 
-void func4()
+void macro4()
 {
     char signature;
     if (signature == 'Y')
         FOUND
     else
-        printf("\nfunc4- Safe... as yet !\t");
+        printf("\nmacro4- Safe... as yet !");
 }
 
 
