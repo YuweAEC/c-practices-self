eb632e4a4efb4ad75c5f76b907e0b30f1cd67ea2
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-08 16:24:50 +0530

    feat: code for pointers

--- daily-code-update/day-9.c
+++ daily-code-update/day-9.c
@@ -1,10 +1,24 @@
 #include<stdio.h>
 #include<conio.h>
 
-void main()
+/*void main()
 {
     int i = 3;
     printf("\nAddress of i = %u", &i);
     printf("\nValue of i = %d", i);
     printf("\nValue of i = %d", *(&i));
+}*/
+
+void main()
+{
+    int i = 3;
+    int *j;
+    j = &i;
+    printf("\nAddress of i = %u", &i);
+    printf("\nAddress of i = %u", j);
+    printf("\nAddress of j = %u", &j);
+    printf("\nValue of j = %u", j);
+    printf("\nValue of i = %d", i);
+    printf("\nValue of i = %d", *(&i));
+    printf("\nValue of i = %d", *j);
 }
