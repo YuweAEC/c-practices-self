baa927962234c165c7ce9fafe20155d116e04346
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-08 16:26:35 +0530

    feat: code for pointer containing pointers address

--- daily-code-update/day-9.c
+++ daily-code-update/day-9.c
@@ -7,7 +7,7 @@
     printf("\nAddress of i = %u", &i);
     printf("\nValue of i = %d", i);
     printf("\nValue of i = %d", *(&i));
-}*/
+}
 
 void main()
 {
@@ -21,4 +21,24 @@ void main()
     printf("\nValue of i = %d", i);
     printf("\nValue of i = %d", *(&i));
     printf("\nValue of i = %d", *j);
+}*/
+
+void main()
+{
+    int i = 3, *j, **k;
+
+    j = &i;
+    k = &j;
+    printf("\nAddress of i = %u", &i);
+    printf("\nAddress of i = %u", j);
+    printf("\nAddress of i = %u", *k);
+    printf("\nAddress of j = %u", &j);
+    printf("\nAddress of j = %u", k);
+    printf("\nAddress of k = %u", &k);
+    printf("\nValue of j = %u", j);
+    printf("\nValue of k = %u", k);
+    printf("\nValue of i = %d", i);
+    printf("\nValue of i = %d", *(&i));
+    printf("\nValue of i = %d", *j);
+    printf("\nValue of i = %d", **k);
 }
