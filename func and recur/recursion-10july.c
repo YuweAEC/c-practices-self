0e2bd25dad0883693fa936122b4490fbe9509530
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-10 23:23:35 +0530

    feat: add code for factorial using recursion1

--- func and recur/recursion-10july.c
+++ func and recur/recursion-10july.c
@@ -1,19 +1,37 @@
 #include<stdio.h>
 #include<conio.h>
 
+//code for factorial using simple function
+
+int factorial ( int ); 
+int rec( int x ); 
+
 void main( ) 
 { 
-    int a, fact ; 
+    int a, fact1, fact ; 
     printf ( "\nEnter any number " ) ; 
     scanf ( "%d", &a ) ; 
-    fact = factorial ( a ) ; 
+    fact1 = factorial ( a ) ; 
+    fact = rec ( a ) ; 
     printf ( "Factorial value = %d", fact ) ; 
 } 
 
-void factorial ( int x ) 
+int factorial ( int x ) 
 { 
     int f = 1, i ; 
     for ( i = x ; i >= 1 ; i-- ) 
     f = f * i ; 
     return ( f ) ; 
 }  
+
+//code to find factorial using recursion
+
+int rec( int x ) 
+{ 
+    int f ; 
+    if ( x == 1 ) 
+    return ( 1 ) ; 
+    else 
+    f = x * rec ( x - 1 ) ; 
+    return ( f ) ; 
+}
