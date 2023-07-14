67adb2fd06253b5db77201a823597e83c1e013b0
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-14 23:39:09 +0530

    feat: add code for floats and double in datatypes

--- func and recur/datatypes.c
+++ func and recur/datatypes.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<conio.h>
-//code to understand chars signed and unsigned rolls in program 
-int main( ) 
+//code to understand chars signed and unsigned rolls in program
+
+void main( ) 
 { 
     char ch = 291 ; 
     char chr ; 
@@ -14,5 +15,32 @@ int main( )
 
     for ( cht = 0 ; cht <= 254 ; cht++ ) 
     printf ( "\n%d %c", cht, cht ) ; 
-    printf ( "\n%d %c", cht, cht ) ; 
-} 
+    printf ( "\n%d %c", cht, cht ) ;  
+    //code floats and double 
+    char c ; 
+    unsigned char d ; 
+    int i ; 
+    unsigned int j ; 
+    short int k ; 
+    unsigned short int l ; 
+    long int m ; 
+    unsigned long int n ; 
+    float x ; 
+    double y ; 
+    long double z ; 
+    /* char */ 
+    scanf ( "%c %c", &c, &d ) ; 
+    printf ( "%c %c", c, d ) ; 
+    /* int */ 
+    scanf ( "%d %u", &i, &j ) ; 
+    printf ( "%d %u", i, j ) ; 
+    /* short int */ 
+    scanf ( "%d %u", &k, &l ) ; 
+    printf ( "%d %u", k, l ) ; 
+    /* long int */ 
+    scanf ( "%ld %lu", &m, &n ) ; 
+    printf ( "%ld %lu", m, n ) ; 
+    /* float, double, long double */ 
+    scanf ( "%f %lf %Lf", &x, &y, &z ) ; 
+    printf ( "%f %lf %Lf", x, y, z ) ; 
+}
