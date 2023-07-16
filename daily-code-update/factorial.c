b722f77f32824f799f8aaf31ae7427d181dd0289
Yuvraj Singh <yuwe18b@gmail.com>
2023-07-16 16:43:57 +0530

    refactor: code to rake user input for factorial

--- daily-code-update/factorial.c
+++ daily-code-update/factorial.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<conio.h>
-#include<fact.h>
+#include<factorial.h>
 //here we include the user defined function and invoke it in main function to find factorial of a number
 void main( ) 
 { 
     int a, fact ; 
     printf ( "\nEnter any number " ) ; 
     scanf ( "%d", &a ) ; 
-    fact = header ( a ) ; 
+    fact = factorial( a ) ; 
     printf ( "Factorial value = %d", fact ) ; 
 }
