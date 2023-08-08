b3be006d3e1b50dc1f43832cad5489bffede1dfb
Yuvraj Singh <yuwe18b@gmail.com>
2023-08-08 22:29:53 +0530

    refactor: update the value and range of an array

--- daily-code-update/array.c
+++ daily-code-update/array.c
@@ -5,17 +5,17 @@ int main( )
 { 
     int avg, sum = 0 ; 
     int i ; 
-    int marks[30] ; /* array declaration */ 
+    int marks[6] ; /* array declaration */ 
     
-    for ( i = 0 ; i <= 29 ; i++ ) 
+    for ( i = 0 ; i <= 5 ; i++ ) 
     { 
     printf ( "\nEnter marks " ) ; 
     scanf ( "%d", &marks[i] ) ; /* store data in array */ 
     } 
     
-    for ( i = 0 ; i <= 29 ; i++ ) 
+    for ( i = 0 ; i <= 5 ; i++ ) 
     sum = sum + marks[i] ; /* read data from an array*/ 
-    avg = sum / 30 ; 
+    avg = sum / 6 ; 
     
     printf ( "\nAverage marks = %d", avg ) ; 
 }
