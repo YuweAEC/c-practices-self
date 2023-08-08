23321775d9467f39741906b798157842682b4cad
Yuvraj Singh <yuwe18b@gmail.com>
2023-08-08 22:23:29 +0530

    feat: add code to find average of a given number

---
+++ daily-code-update/array.c
@@ -0,0 +1,18 @@
+int main( ) 
+{ 
+    int avg, sum = 0 ; 
+    int i ; 
+    int marks[30] ; /* array declaration */ 
+    
+    for ( i = 0 ; i <= 29 ; i++ ) 
+    { 
+    printf ( "\nEnter marks " ) ; 
+    scanf ( "%d", &marks[i] ) ; /* store data in array */ 
+    } 
+    
+    for ( i = 0 ; i <= 29 ; i++ ) 
+    sum = sum + marks[i] ; /* read data from an array*/ 
+    avg = sum / 30 ; 
+    
+    printf ( "\nAverage marks = %d", avg ) ; 
+}
