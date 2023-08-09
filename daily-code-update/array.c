dfcfa64ef54a995820b9e83bad5a4f6d45106cec
Yuvraj Singh <yuwe18b@gmail.com>
2023-08-09 23:33:40 +0530

    refactor: make menu driven and add code for call by value in an array

--- daily-code-update/array.c
+++ daily-code-update/array.c
@@ -1,59 +1,87 @@
-#include<stdio.h>
+#include <stdio.h>
+#include<stdlib.h>
 #include<conio.h>
 
-int average();
-int callByValue();
+// Function prototypes
+
+void demonstrateArray();
+void demonstrateCallByValue();
+void displayValue(int value);
 
 int main()
 {
     int choice;
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-    switch(choice)
+    
+    while (1)
     {
-        case 1:
-            average();
-            break;
-        case 2:
-            callByValue();
-            break;
-        default:
-            printf("Invalid choice");
+        printf("\nEnter your choice:\n");
+        printf("1. Demonstrate Array\n");
+        printf("2. Demonstrate Call By Value\n");
+        printf("3. Display Value\n");
+        printf("4. Exit\n");
+        scanf("\n%d", &choice);
+
+        switch (choice)
+        {
+            case 1:
+                demonstrateArray();
+                break;
+            case 2:
+                demonstrateCallByValue();
+                break;
+            case 3:
+                int value;
+                printf("Enter a value: ");
+                scanf("%d", &value);
+                displayValue(value);
+                break;
+            case 4:
+                printf("Exiting...\n");
+                return 0;
+            default:
+                printf("Invalid choice\n");
+        }
     }
+
     return 0;
 }
 
 // Demonstration of array
 
-int average()
-{ 
-    int avg, sum = 0 ; 
-    int i ; 
-    int marks[6] ; /* array declaration */ 
-    
-    for ( i = 0 ; i <= 5 ; i++ ) 
-    { 
-    printf ( "\nEnter marks " ) ; 
-    scanf ( "%d", &marks[i] ) ; /* store data in array */ 
-    } 
-    
-    for ( i = 0 ; i <= 5 ; i++ ) 
-    sum = sum + marks[i] ; /* read data from an array*/ 
-    avg = sum / 6 ; 
-    
-    printf ( "\nAverage marks = %d", avg ) ; 
+void demonstrateArray()
+{
+    int avg, sum = 0;
+    int i;
+    int marks[6]; // Array declaration
+
+    for (i = 0; i < 6; i++)
+    {
+        printf("Enter marks: ");
+        scanf("%d", &marks[i]); // Store data in array
+    }
+
+    for (i = 0; i < 6; i++)
+        sum += marks[i]; // Read data from the array
+
+    avg = sum / 6;
+
+    printf("Average marks = %d\n", avg);
 }
 
-/* Demonstration of call by value */
+// Demonstration of call by value
 
-int callByValue()
-{ 
-    int i ; 
-    int marks[ ] = { 55, 65, 75, 56, 78, 78, 90 } ; 
-    for ( i = 0 ; i <= 6 ; i++ ) 
-    display ( marks[i] ) ; 
-} 
-display ( int m ) 
-{ 
-    printf ( "%d ", m ) ; 
+void demonstrateCallByValue()
+{
+    int i;
+    int marks[] = {55, 65, 75, 56, 78, 78, 90};
+
+    for (i = 0; i < 7; i++)
+        displayValue(marks[i]);
+}
+
+// Display function definition
+
+void displayValue(int value)
+{
+    printf("%d ", value);
 }
