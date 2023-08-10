48f70af08d3889862479dba454bedc803a1576e6
Yuvraj Singh <yuwe18b@gmail.com>
2023-08-10 23:33:43 +0530

    refactor: add comment to show increment

--- daily-code-update/array2.c
+++ daily-code-update/array2.c
@@ -22,21 +22,21 @@ int display( int *n ) // function invocation
 
 int pointersAndArrays( ) // function invocation
 { 
-    int i = 3, *x ; 
+    int i = 3, *x ; // declare i and x
     float j = 1.5, *y ; 
     char k = 'c', *z ; 
     printf ( "\n\n\nValue of i = %d", i ) ; 
     printf ( "\nValue of j = %f", j ) ; 
     printf ( "\nValue of k = %c", k ) ; 
-    x = &i ; 
-    y = &j ; 
-    z = &k ; 
+    x = &i ; // x stores the address of i 
+    y = &j ; // y stores the address of j
+    z = &k ;  // z stores the address of k
     printf ( "\nOriginal address in x = %u", x ) ; 
     printf ( "\nOriginal address in y = %u", y ) ; 
     printf ( "\nOriginal address in z = %u", z ) ; 
-    x++ ; 
-    y++ ; 
-    z++ ; 
+    x++ ; // increment x by 1 
+    y++ ; // increment y by 1
+    z++ ; // increment z by 1
     printf ( "\nNew address in x = %u", x ) ; 
     printf ( "\nNew address in y = %u", y ) ; 
     printf ( "\nNew address in z = %u", z ) ; 
