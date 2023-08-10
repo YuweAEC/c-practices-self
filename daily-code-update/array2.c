6b2d403654404d6836685ad5cf8bd2460d8ff4e1
Yuvraj Singh <yuwe18b@gmail.com>
2023-08-10 23:11:08 +0530

    feat: add code for call by reference in an array

---
+++ daily-code-update/array2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int display( ); 
+
+int main( ) 
+{ 
+    int i ; 
+    int marks[ ] = { 55, 65, 75, 56, 78, 78, 90 } ; 
+    for ( i = 0 ; i <= 6 ; i++ ) 
+    display( &marks[i] ) ; 
+} 
+
+int display( int *n ) 
+{ 
+    printf ( "%d ", *n ) ; 
+} 
