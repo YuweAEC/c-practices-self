e63de60b1fca50960c25c6632e8200b699e98c39
Yuvraj Singh <yuwe18b@gmail.com>
2023-08-12 23:49:08 +0530

    feat: add 2nd method to access array elements

--- daily-code-update/array3.c
+++ daily-code-update/array3.c
@@ -1,11 +1,25 @@
 #include<stdio.h>
-#include<canio.h>
+#include<conio.h>
 #include<stdlib.h>
+
+int accessArrayElement( ); // prototype declare
 int arrayElement( ); // prototype declare
 
 int main()
 {
+    accessArrayElement( ); // function call
     arrayElement( ); // function call
+}
+
+int accessArrayElement( ) 
+{ 
+    int num[ ] = { 24, 34, 12, 44, 56, 17 } ; 
+    int i ; 
+    for ( i = 0 ; i <= 5 ; i++ ) 
+    { 
+        printf ( "\naddress = %u ", &num[i] ) ; 
+        printf ( "element = %d", num[i] ) ; 
+    } 
 }
 
 int arrayElement( ) 
@@ -16,7 +30,7 @@ int arrayElement( )
     
     for( i = 0 ; i <= 5 ; i++ ) 
     { 
-        printf ( "\naddress = %u ", j ) ; 
+        printf ( "\n\naddress = %u ", j ) ; 
         printf ( "element = %d", *j ) ; 
         j++ ; /* increment pointer to point to next location */ 
     } 
