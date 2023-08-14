74ab4b4509255839906fdd45363aeafbaa0ec46e
Yuvraj Singh <yuwe18b@gmail.com>
2023-08-14 22:54:35 +0530

    feat: add code to access array elements in different methods

--- daily-code-update/array-4.c
+++ daily-code-update/array-4.c
@@ -2,16 +2,28 @@
 #include<conio.h>
 
 /* Demonstration of passing an entire array to a function */ 
+
 int display( int *, int ) ;
 
 int main( ) 
 { 
     int num[ ] = { 24, 34, 12, 44, 56, 17 } ; 
+    int i ; 
+    
+    for ( i = 0 ; i <= 5 ; i++ ) 
+    { 
+        printf ( "\naddress = %u ", &num[i] ) ; 
+        printf ( "element = %d %d ", num[i], *( num + i ) ) ; 
+        printf ( "%d %d", *( i + num ), i[num] ) ; 
+    }
+    
     display ( &num[0], 6 ) ; 
 } 
+
 int display( int *j, int n ) 
 { 
     int i ; 
+    
     for ( i = 0 ; i <= n - 1 ; i++ ) 
     { 
         printf ( "\nelement = %d", *j ) ;
