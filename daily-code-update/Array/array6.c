82987c2a094bce1e54aa71978942619c1d1d82dc
Yuvraj Singh <yuwe18b@gmail.com>
2023-08-16 23:29:03 +0530

    feat: add code for pointers notation to access 2-d array elements

--- daily-code-update/Array/array6.c
+++ daily-code-update/Array/array6.c
@@ -2,14 +2,16 @@
 #include <conio.h>
 
 int twoDimensionalArray( ); // prototype declare
+int pointers2dArray( ); // prototype declare
 
 int main ()
 {
     twoDimensionalArray( ); // function call
+    pointers2dArray( ); // function call
     return 0;
 }
 
-/* Demo: 2-D array is an array of arrays */ 
+/* 2-D array is an array of arrays */ 
 
 int twoDimensionalArray( )  // function invocation 
 { 
@@ -26,3 +28,26 @@ int twoDimensionalArray( )  // function invocation
     for ( i = 0 ; i <= 3 ; i++ )  // 2-D array traversal 
         printf ( "\nAddress of %d th 1-D array = %u", i, s[i] ) ;  // address of each 1-D array  
 }
+
+/* Pointer notation to access 2-D array elements */ 
+
+int pointers2dArray( ) 
+{ 
+    int s[4][2] = 
+    { 
+        { 1234, 56 }, 
+        { 1212, 33 }, 
+        { 1434, 80 }, 
+        { 1312, 78 } 
+    } ; 
+    
+    int i, j ; 
+    
+    for ( i = 0 ; i <= 3 ; i++ ) 
+    { 
+        printf ( "\n" ) ; 
+        for ( j = 0 ; j <= 1 ; j++ ) 
+        printf ( "%d ", *( *( s + i ) + j ) ) ; 
+    } 
+}
+
