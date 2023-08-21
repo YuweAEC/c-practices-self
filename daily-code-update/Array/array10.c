4fc1d03274cd5fadb152c8e792564f6b0114dfef
Yuvraj Singh <yuwe18b@gmail.com>
2023-08-21 23:51:27 +0530

    feat: add code print datatypes in structure

--- daily-code-update/Array/array10.c
+++ daily-code-update/Array/array10.c
@@ -2,10 +2,12 @@
 #include<conio.h>
 
 void singleCharacterArray( );
+void structureBook();
 
 int main()
 {
     singleCharacterArray();
+    structureBook();
     return 0;
 }
 
@@ -22,4 +24,23 @@ void singleCharacterArray( )
     printf ( "\nAnd this is what you entered\n" ) ; 
     for ( i = 0 ; i <= 2 ; i++ ) 
     printf ( "%c %f %d\n", name[i], price[i], pages[i] ); 
+} 
+
+void structureBook( ) 
+{ 
+    struct book 
+    { 
+    char name ; 
+    float price ; 
+    int pages ; 
+    } ; 
+    struct book b1, b2, b3 ; 
+    printf ( "\nEnter names, prices & no. of pages of 3 books\n" ) ; 
+    scanf ( "%c %f %d", &b1.name, &b1.price, &b1.pages ) ; 
+    scanf ( "%c %f %d", &b2.name, &b2.price, &b2.pages ) ; 
+    scanf ( "%c %f %d", &b3.name, &b3.price, &b3.pages ) ; 
+    printf ( "\nAnd this is what you entered" ) ; 
+    printf ( "\n%c %f %d", b1.name, b1.price, b1.pages ) ; 
+    printf ( "\n%c %f %d", b2.name, b2.price, b2.pages ) ; 
+    printf ( "\n%c %f %d", b3.name, b3.price, b3.pages ) ; 
 } 
