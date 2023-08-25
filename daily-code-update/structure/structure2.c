376af04d6f7c7787821d096778d7c5bef7bcc51b
Yuvraj Singh <yuwe18b@gmail.com>
2023-08-25 22:52:41 +0530

    feat: initial changes

---
+++ daily-code-update/structure/structure2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+/* write a program to read and display the information of 100 books using an array of structures */
+
+void arrayStructures( ); // function prototype 
+
+int main()
+{
+    arrayStructures();
+    return 0;
+}
+
+/* Usage of an array of structures */ 
+
+void arrayStructures( ) 
+{ 
+    struct book 
+    { 
+    char name ; 
+    float price ; 
+    int pages ; 
+    } ; 
+    struct book b[100] ; 
+    int i ; 
+    for ( i = 0 ; i <= 99 ; i++ ) 
+    { 
+    printf ( "\nEnter name, price and pages " ) ; 
+    scanf ( "%c %f %d", &b[i].name, &b[i].price, &b[i].pages ) ; 
+    } 
+    for ( i = 0 ; i <= 99 ; i++ ) 
+    printf ( "\n%c %f %d", b[i].name, b[i].price, b[i].pages ) ; 
+} 
+void linkfloat( ) 
+{ 
+    float a = 0, *b ; 
+    b = &a ; /* cause emulator to be linked */ 
+    a = *b ; /* suppress the warning - variable not used */ 
+} 
