ac4bc07ae84fb5cdd6e76513e7052f84ac0de92e
Yuvraj Singh <yuwe18b@gmail.com>
2023-08-26 22:11:23 +0530

    refactor: initial commit

--- daily-code-update/structure/structure3.c
+++ daily-code-update/structure/structure3.c
@@ -3,11 +3,15 @@
 #include<stdlib.h>
 #include<string.h>
 
+/* program to copy one structure to another structure */
+
 void structure( ); // function prototype
+void nestedStructure( );
 
 int main()
 {
     structure();
+    nestedStructure();
     return 0;}
 
 void structure( ) 
@@ -29,4 +33,25 @@ void structure( )
     printf ( "\n%s %d %f", e1.name, e1.age, e1.salary ) ; 
     printf ( "\n%s %d %f", e2.name, e2.age, e2.salary ) ; 
     printf ( "\n%s %d %f", e3.name, e3.age, e3.salary ) ; 
+} 
+
+void nestedStructure( )
+{ 
+    struct address 
+    { 
+        char phone[15] ; 
+        char city[25] ; 
+        int pin ; 
+    } ; 
+
+    struct emp 
+    { 
+        char name[25] ; 
+        struct address a ; 
+    } ; 
+
+    struct emp e = { "jeru", "531046", "nagpur", 10 }; 
+
+    printf ( "\nname = %s phone = %s", e.name, e.a.phone ) ; 
+    printf ( "\ncity = %s pin = %d", e.a.city, e.a.pin ) ; 
 } 
