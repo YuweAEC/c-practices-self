578e80d1fc92adee7c54afc5e970e758c02beb02
Yuvraj Singh <yuwe18b@gmail.com>
2023-09-06 23:12:30 +0530

    feat: add code to print arra elements using pointers and display names using pointers and strings

---
+++ daily-code-update/strings/pointers_strings.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+void arrayPointers();
+void stringPointers();
+
+int main()
+{
+    arrayPointers();
+    stringPointers();
+    return 0;
+}
+
+void arrayPointers( ) 
+{ 
+    char name[ ] = "Klinsman" ; 
+    char *ptr ; 
+    
+    ptr = name ; /* store base address of string */ 
+    
+    while ( *ptr != '\0' ) 
+    { 
+        printf ( "%c", *ptr ) ; 
+        ptr++ ; 
+    } 
+} 
+
+void stringPointers( ) 
+{ 
+    char str1[ ] = "Hello" ; 
+    char str2[10] ; 
+    char *s = "Good Morning" ; 
+    char *q ; 
+    q = s ; /* works */ 
+} 
