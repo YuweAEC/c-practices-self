b7a0e24009378252e4316a01b69d590309ce98fe
Yuvraj Singh <yuwe18b@gmail.com>
2023-09-08 22:40:41 +0530

    feat: add code for user defined string function

--- daily-code-update/strings/string-library.c
+++ daily-code-update/strings/string-library.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include<conio.h>
 #include<string.h>
+
 // program to illustrate the use of string library functions.
+/*
 int main( ) 
 { 
     char arr[ ] = "Bamboozled" ; 
@@ -10,4 +12,30 @@ int main( )
     len2 = strlen ( "Humpty Dumpty" ) ; 
     printf ( "\nstring = %s length = %d", arr, len1 ) ; 
     printf ( "\nstring = %s length = %d", "Humpty Dumpty", len2 ) ; 
+}
+*/
+
+/* using user defined */ 
+
+int lengthString(const char *s);
+
+int main() 
+{ 
+    char arr[] = "Bamboozled"; 
+    int len1, len2; 
+    len1 = lengthString(arr); 
+    len2 = lengthString("Humpty Dumpty"); 
+    printf("\nstring = %s length = %d", arr, len1); 
+    printf("\nstring = %s length = %d", "Humpty Dumpty", len2); 
+}
+
+int lengthString(const char *s) 
+{ 
+    int length = 0; 
+    while (*s != '\0') 
+    { 
+        length++; 
+        s++; 
+    } 
+    printf("\nLength = %d", length); 
 }
