3f270e29b99c5128ba78c1a82f7a2f3f4af6ba63
Yuvraj Singh <yuwe18b@gmail.com>
2023-09-10 23:11:45 +0530

    refactor: add code for strcat

--- daily-code-update/strings/string-library.c
+++ daily-code-update/strings/string-library.c
@@ -22,6 +22,15 @@ int main( )
     printf ( "\nsource string = %s", source ) ; 
     printf ( "\ntarget string = %s", target ) ; 
 }
+
+main( ) 
+{ 
+    char source[ ] = "Folks!" ; 
+    char target[30] = "Hello" ; 
+    strcat ( target, source ) ; 
+    printf ( "\nsource string = %s", source ) ; 
+    printf ( "\ntarget string = %s", target ) ; 
+}
 */
 
 /* using user defined string length function */ 
