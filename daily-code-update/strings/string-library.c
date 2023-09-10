f21d4652c606c9c370df41ad5cb913892206e497
Yuvraj Singh <yuwe18b@gmail.com>
2023-09-10 23:20:22 +0530

    refactor: add code for strcmp()

--- daily-code-update/strings/string-library.c
+++ daily-code-update/strings/string-library.c
@@ -31,6 +31,17 @@ int main( )  // program to illustrate the use of strcat ( )
     printf ( "\nsource string = %s", source ) ; 
     printf ( "\ntarget string = %s", target ) ; 
 }
+
+int main( ) 
+{ 
+    char string1[ ] = "Jerry" ; 
+    char string2[ ] = "Ferry" ; 
+    int i, j, k ; 
+    i = strcmp ( string1, "Jerry" ) ; 
+    j = strcmp ( string1, string2 ) ; 
+    k = strcmp ( string1, "Jerry boy" ) ; 
+    printf ( "\n%d %d %d", i, j, k ) ; 
+} 
 */
 
 /* using user defined string length function */ 
